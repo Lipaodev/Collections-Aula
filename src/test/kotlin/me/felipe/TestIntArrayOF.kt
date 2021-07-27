55dd6815d92b134003469dcd4aff6b356e2de073
Felipe <https://github.com/Lipaodev/calc.git>
2021-07-27 08:59:09 -0300

    TestIntArrayOF (Opção de dimensionamento do array que facilita a inserção de novos elementos).

---
+++ src/test/kotlin/me/felipe/TestIntArrayOF.kt
@@ -0,0 +1,19 @@
+package me.felipe
+
+ fun main () {
+    val values =  intArrayOf (12,2,4,8,10,12) //Opção de dimensionamento do array, podendo ser alterado mesmo sendo constante.
+
+
+        values.forEach {
+            println(it)
+        }
+
+
+           println(  )
+
+            values.sort()
+            values.forEach {
+                println(it)
+            }
+
+ }
