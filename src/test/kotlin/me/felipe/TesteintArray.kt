7eafd7d240739df29d1c7c10f6fade9b94795841
Felipe <https://github.com/Lipaodev/calc.git>
2021-07-27 08:40:43 -0300

    IntArray Test Class (Aula explicativa como printar de diversas formas um mesmo valor.  )

--- src/test/kotlin/me/felipe/TesteintArray.kt
+++ src/test/kotlin/me/felipe/TesteintArray.kt
@@ -9,11 +9,30 @@ fun main(){
     values [3] = 10
     values [4] = 25
 
-    for (valor in values)
-    println(valor)
+    for (valor in values) {
+        println(valor)
+        //Forma padrao de referenciar a variavel e chamar ela
+    }
 
-    values.forEach {
-        println(it)
+    println("--------------------")
+    values.forEach { valor ->
+        println(valor)
+        //Printa o valor da variavel valor sem precisar chamar a val values
+    }
+
+        println("-----------------")
+        for (index in values.indices) {
+            println(values[index])
+            //Evidencia o índice, e após o valor dos índices
+        }
+
+        println("-----------------")
+        values.sort()
+        for (valor in values){
+            println(valor)
+            //Ordena os valores do menor para o maior com a função sort!!
+
+
     }
 
 }
