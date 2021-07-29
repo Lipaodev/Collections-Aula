f0657a13ecfe2466099d73b2c083e6971990438f
Felipe <https://github.com/Lipaodev/calc.git>
2021-07-29 19:38:40 -0300

    TestListOf.kt (Foi realizado teste de listOF para atribuir as posições de cada funcionário na memória. Também foi realizado um find, sortedBy, que é organização crescente dos objetos e o groupBy que é o agrupamento de objetos correspondentes ao valor solicitado no código.)

--- src/test/kotlin/me/felipe/TestListOf.kt
+++ src/test/kotlin/me/felipe/TestListOf.kt
@@ -1,5 +1,43 @@
 package me.felipe
 
-fun main(){
+fun main() {
+    val Joao = funcionario(nome = "Joao", salario = 2000.0, tipoContratacao = "CLT")
+    val Pedro = funcionario(nome = "Pedro", salario = 1000.0, tipoContratacao = "PJ")
+    val Maria = funcionario(nome = "Maria", salario = 4000.0, tipoContratacao = "CLT") //Instanciado tres objetos com valores especificos.
+
+    val funcionarios = listOf(Joao, Pedro, Maria) //Atribuiu a uma lista
+
+    funcionarios.forEach { println(it)} //Iterou a lista
+
+    println("_______")
+    println(funcionarios.find { it.nome == "Maria" }) //Buscou somente o objeto Maria
+
+    println("_______")
+    funcionarios
+        .sortedBy { it.salario}//Organizado de forma crescente pelo salário.
+        .forEach { println(it) } //Tem a responsabilidade de finalizar as informações.
+
+    println("_______")
+
+    funcionarios
+        .groupBy { it.tipoContratacao} //Agrupa pelo tipo de contratacao
+        .forEach { println(it) }    //Tem a responsabilidade de finalizar as informações.
+
+
 
 }
+
+    data class funcionario(
+        val nome: String,
+        val salario: Double, //Foi criado um data class com as variaveis
+        val tipoContratacao: String,
+
+        ) {
+        override fun toString(): String =
+            """
+            Nome:    $nome
+            Salario: $salario 
+            
+        """.trimIndent() // Função do kotlin que permite voce imprimir os valores de forma mais organizada na tela.
+
+    }
