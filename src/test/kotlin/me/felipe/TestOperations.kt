e7b825be65c72ba9659c60a8378fe258c710d884
Felipe <https://github.com/Lipaodev/calc.git>
2021-07-29 10:06:49 -0300

    TestOperations.kt (Teste de maximo, minimo e media alem de filtro para buscar valores e expressoes em especifico listado no banco de dados, foram acrescidos operações de maximo, minimo, media, filtro para contagem, busca e validação do valores especificados)

--- src/test/kotlin/me/felipe/TestOperations.kt
+++ src/test/kotlin/me/felipe/TestOperations.kt
@@ -7,12 +7,29 @@ fun main(){
         println(salario)
 
         println("-----------------")
-        println("Maior Salario: ${salarios.max()}")
-        println("Menor Salario: ${salarios.min()}")
-        println("Media Salario: ${salarios.average()}")
+        println("Maior Salario: ${salarios.max()}") //Salarios maximo no range
+        println("Menor Salario: ${salarios.min()}") //Salario minimo no range
+        println("Media Salario: ${salarios.average()}") //Media salarial
 
-        val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+        val salariosMaiorQue2500 = salarios.filter { it > 2500.0 } //Filtro para busca de salario maior que o valor especificado
         println("-----------------")
-        salariosMaiorQue2500.forEach {println(it)}
+        salariosMaiorQue2500.forEach {println(it)} //Imprime o valor do salario maior que o especificado
+
+        println("-----------------")
+        println(salarios.count{ it in 2000.0..5000.0}) //Conta a quantidade de salários no range espeficado.
+
+        println("-----------------")
+        println(salarios.find { it == 2250.0}) //Busca exatamente o valor especificado e exibe valor
+        println(salarios.find { it == 250.0}) //Busca exatamente o valor especificado  e exibe o valor
+
+        println("-----------------")
+        println(salarios.any { it == 1000.0}) //Busca exatamente o valor especificado e validade de se é verdadeiro ou não
+        println(salarios.any { it == 500.0}) //Busca exatamente o valor especificado e validade de se é verdadeiro ou não
+
+
+
+
+
+
     }
 }
